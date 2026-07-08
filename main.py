fc3b62e580783af7a779c1dcbc88db481b7beff1
Roland <cookroland56@gmail.com>
2026-07-08 14:21:29 -0400

    Added put function

--- main.py
+++ main.py
@@ -23,3 +23,6 @@ def read_item(item_id: int, q: Union[str, None] = None):
 
 
 # Added PUT route that accepts a path parameter and a JSON body
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_id": item_id}
